98736932f07c80f1628bee314722e6c6a5d1bbcd
Armando Jr <elfiservice@hotmail.com>
2023-12-10 12:39:23 -0300

    Added the new Pages initial strucutre

---
+++ Pages/ProfilePage.cs
@@ -0,0 +1,15 @@
+namespace MauiApp1;
+
+public class ProfilePage : ContentPage
+{
+	public ProfilePage()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
