7ccfd3914f00ce6669582640c53b752540788d59
Armando Jr <elfiservice@hotmail.com>
2023-12-17 10:40:41 -0300

    moved AThread from HomePage to Models folder

---
+++ Models/AThread.cs
@@ -0,0 +1,25 @@
+﻿namespace ThreadsApp.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+	public partial class AThread : ObservableObject
+	{
+		[ObservableProperty]
+		string user;
+		[ObservableProperty]
+		string image;
+		[ObservableProperty]
+		string message;
+		[ObservableProperty]
+		[NotifyPropertyChangedFor(nameof(HasLikes))]
+		int likes;
+		[ObservableProperty]
+		[NotifyPropertyChangedFor(nameof(HasReplies))]
+		int replies;
+		[ObservableProperty]
+		bool isVerified;
+		[ObservableProperty]
+		string timeAgo;
+
+		public bool HasReplies => Replies > 0;
+		public bool HasLikes => Likes > 0;
+	}
