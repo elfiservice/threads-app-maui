32f47971c900351f011dedf7ccdd8d39aaffefa3
Armando Jr <elfiservice@hotmail.com>
2023-12-21 07:36:11 -0300

    provided user view cell to create template to list view

--- Pages/SearchPage.cs
+++ Pages/SearchPage.cs
@@ -1,3 +1,12 @@
+using ThreadsApp.Helpers;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+using Microsoft.Maui.Controls.PlatformConfiguration;
+using ListView = Microsoft.Maui.Controls.ListView;
+using ThreadsApp.Models;
+using CommunityToolkit.Maui.Markup;
+using static CommunityToolkit.Maui.Markup.GridRowsColumns;
+using ThreadsApp.Controls;
+
 namespace ThreadsApp.Pages;
 
 public class SearchPage : BasePage
@@ -11,6 +20,8 @@ public class SearchPage : BasePage
             CancelButtonColor = Colors.Black,
             Keyboard = Keyboard.Text
         });
+
+		On<iOS>().SetLargeTitleDisplay(LargeTitleDisplayMode.Always);
 	}
 
     public override void Build()
@@ -20,9 +31,81 @@ public class SearchPage : BasePage
         var listView = new ListView(ListViewCachingStrategy.RecycleElement);
         listView.HasUnevenRows = true;
         listView.SelectionMode = ListViewSelectionMode.None;
-        // listView.ItemsSource = ThreadsGenerator.CreateUsers();
-        // listView.ItemTemplate = new DataTemplate(typeof(UserViewCell));
+        listView.ItemsSource = ThreadsGenerator.CreateUsers();
+        listView.ItemTemplate = new DataTemplate(typeof(UserViewCell));
 
         Content = listView;
+    }
+
+	public class UserViewCell : ViewCell
+    {
+        Button followButton;
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            if (BindingContext is not AUser user)
+                return;
+
+            followButton
+            .Text(user.IsFollowing ? "Unfollow" : "Follow");
+        }
+        public UserViewCell()
+        {
+
+            followButton = new Button
+            {
+                BorderColor = Colors.LightGray,
+                BorderWidth = 1,
+                CornerRadius = 12
+            }
+				.Bold()
+				.Padding(20, 0)
+				.RowSpan(2)
+				.Column(2)
+				.Height(33)
+				.BackgroundColor(Colors.Transparent)
+				.TextColor(Colors.Black)
+				.CenterVertical();
+
+            View = new Grid
+            {
+                ColumnDefinitions = Columns.Define(Auto, Star, Auto),
+                RowDefinitions = Rows.Define(Auto, Auto, Auto),
+                ColumnSpacing = 10,
+                RowSpacing = 4,
+                Children =
+                {
+                    new Image()
+                        .Bind(Image.SourceProperty, nameof(AUser.Image))
+                        .Width(35).Height(35)
+                        .Row(0).Column(0)
+                        .RowSpan(3)
+                        .CenterHorizontal()
+                        .Top()
+                        .Margin(new Thickness(0, 5, 0, 0))
+                        .Aspect(Aspect.AspectFill),
+                    new UserView()
+                        .Row(0).Column(1),
+                    new Label()
+                        .Bind(Label.TextProperty, nameof(AUser.DisplayName))
+                        .TextColor(Colors.LightGray)
+                        .Column(1)
+                        .Row(1),
+                    new HorizontalStackLayoutSpaced()
+                    {
+                        new Image()
+                        .Source("profilecircle.png")
+                        .Width(12).Height(12)
+                        .CenterHorizontal()
+                        .Bind(Image.IsVisibleProperty, nameof(AUser.HasSimilarFollowers)),
+                        // new Label()
+                        //     .Bind(Label.TextProperty, nameof(AUser.FollowersDisplay))
+
+                    }.Column(1)
+                    .Row(2),
+                    followButton
+                }
+            }.Padding(10, 10);
+        }
     }
 }
