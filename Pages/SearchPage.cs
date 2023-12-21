4b1e9fcb448b4cbaf368d87828ba6efe05ef796b
Armando Jr <elfiservice@hotmail.com>
2023-12-21 07:14:27 -0300

    provided base structure to SearchPage

--- Pages/SearchPage.cs
+++ Pages/SearchPage.cs
@@ -1,15 +1,28 @@
 namespace ThreadsApp.Pages;
 
-public class SearchPage : ContentPage
+public class SearchPage : BasePage
 {
 	public SearchPage()
 	{
-		Content = new VerticalStackLayout
-		{
-			Children = {
-				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Search Page"
-				}
-			}
-		};
+        Shell.SetSearchHandler(this, new SearchHandler
+        {
+            Placeholder = "Search",
+            ShowsResults = false,
+            CancelButtonColor = Colors.Black,
+            Keyboard = Keyboard.Text
+        });
 	}
+
+    public override void Build()
+    {
+        Title = "Search";
+
+        var listView = new ListView(ListViewCachingStrategy.RecycleElement);
+        listView.HasUnevenRows = true;
+        listView.SelectionMode = ListViewSelectionMode.None;
+        // listView.ItemsSource = ThreadsGenerator.CreateUsers();
+        // listView.ItemTemplate = new DataTemplate(typeof(UserViewCell));
+
+        Content = listView;
+    }
 }
